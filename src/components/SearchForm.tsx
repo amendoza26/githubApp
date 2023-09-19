2a23f783b6ac525d68590f8bdcbf01aab521034d
Alex Mendoza <amendoza807@gmail.com>
2023-09-18 23:21:07 -0500

    feat: add Search user component

--- src/components/SearchForm.tsx
+++ src/components/SearchForm.tsx
@@ -20,11 +20,12 @@ const SearchForm = ({ onSearch }: SearchProps) => {
     <form onSubmit={handleSubmit}>
       <input
         type="text"
-        placeholder="Buscar usuarios"
+        placeholder="Search other users"
         value={query}
         onChange={handleInputChange}
+        className="border border-gray-300 rounded-lg py-2 px-4 focus:ring focus:ring-blue-200 shadow-md flex-grow"
       />
-      <button type="submit">Buscar</button>
+      <button type="submit" className="bg-blue-500 hover:bg-blue-600 text-white font-semibold py-2 px-4 rounded-md shadow-md transition-all duration-300 ml-2">Search</button>
     </form>
   );
 };
