2d6376816fde124ad5a59016602d2791e255f4d5
Alex Mendoza <amendoza807@gmail.com>
2023-09-19 00:21:07 -0500

    feat: add github commit service

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,37 +1,57 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { getUserData, getUserRepos } from '../services/github.service';
+import CommitList from '../components/CommitList';
 import SearchForm from '../components/SearchForm';
 
-    type Repo = {
-        id: number;
-        name: string;
-    }
+type Repo = {
+    id: number;
+    name: string;
+}
 
-    type UserData = {
-        name: string; 
-        login: string; 
-    };
+type UserData = {
+    name: string; 
+    login: string; 
+    html_url: string;
+};
 
 const Home = () => {
     const [userData, setUserData] = useState<UserData>({ 
-        name: '',
-        login: '',
-      });
+      name: '',
+      login: '',
+      html_url: '',
+    });
     const [repos, setRepos] = useState<Repo[]>([]);
     const [showRepos, setShowRepos] = useState(false);
     const [loading, setLoading] = useState(false);
-    
+    const [selectedRepo, setSelectedRepo] = useState<string | null>(null);
+
     const handleSearch = async (username: string) => {
-        if (username.trim() === '') {
-          return;
-        }
+      if (username.trim() === '') {
+        return;
+      }
+  
+      setLoading(true);
+  
+      try {
+        const data = await getUserData(username);
+        setUserData(data);
+
+        const repos = await getUserRepos(username);
+        setRepos(repos);
+      } catch (error) {
+        console.error(error);
+      } finally {
+        setLoading(false);
+      }
+  };
     
-        setLoading(true);
-    
+    useEffect(() => {
+      const fetchData = async () => {
         try {
+          const username = 'amendoza26';
           const data = await getUserData(username);
           setUserData(data);
-
+  
           const repos = await getUserRepos(username);
           setRepos(repos);
         } catch (error) {
@@ -39,38 +59,78 @@ const Home = () => {
         } finally {
           setLoading(false);
         }
-    };
+      };
+  
+      fetchData();
+    }, []);
 
     const handleShowRepos = () => {
-        setShowRepos(true);
+      setShowRepos(true);
+    };
+
+    const handleSelectRepo = (repoName: string) => {
+      setSelectedRepo(repoName);
     };
   
-    return (
-        <div>
-            <h1>Buscador de usuarios de GitHub</h1>
-            <SearchForm onSearch={handleSearch} />
-
-            {loading && <p>Cargando...</p>}
-
-            {userData.name && (
-                <div>
-                    <h2>{userData.name}</h2>
+      return (
+        <div className="min-h-screen">
+          <div className="max-w-7xl mx-auto py-6 sm:px-6 lg:px-8">
+            <div className="md:flex md:items-center md:justify-between">
+              <div className="flex-1 min-w-0">
+                <h1 className="text-3xl font-bold leading-7 text-gray-900 sm:text-4xl sm:truncate text-center">
+                  List of Commits App
+                </h1>
+              </div>
+            </div>
+          </div>
+          <main className="max-w-7xl mx-auto py-6 sm:px-6 lg:px-8">
+            <div className="px-4 py-6 sm:px-0">
+              <SearchForm onSearch={handleSearch} />
+    
+              {loading && <p className="text-center mt-4">Loading...</p>}
+    
+              {userData.name && (
+                <div className="mt-8 md:flex space-x-10">
+                  <div className="md:w-1/2">
+                    <h2 className="text-2xl font-semibold text-gray-900">Initial user: {userData.name}</h2>
+                    <h3><a>{userData.html_url}</a></h3>
                     {showRepos ? (
-                        <>
-                            <h3>Repositorios publicos</h3>
-                            <ul>
-                                {repos.map((repo) => (
-                                <li key={repo.id}>{repo.name}</li>
-                                ))}
-                            </ul>
-                        </>
+                      <>
+                        <h3 className="mt-6 text-xl font-semibold text-gray-900">Repositories</h3>
+                        <ul className="mt-4 space-y-2">
+                          {repos.map((repo) => (
+                            <li key={repo.id} className="flex items-center justify-between">
+                              <span className="hover:underline">{repo.name}</span>
+                              <button
+                                onClick={() => handleSelectRepo(repo.name)}
+                                className="px-3 py-1 text-sm font-medium text-white bg-blue-500 rounded hover:bg-blue-600 focus:outline-none focus:bg-blue-600"
+                              >
+                                Repository Commits
+                              </button>
+                            </li>
+                          ))}
+                        </ul>
+                      </>
                     ) : (
-                        <button onClick={handleShowRepos}>Ver Repos</button>
+                      <button
+                        onClick={handleShowRepos}
+                        className="mt-6 px-4 py-2 text-sm font-medium text-white bg-blue-500 rounded hover:bg-blue-600 focus:outline-none focus:bg-blue-600"
+                      >
+                        List of Repos
+                      </button>
                     )}
+                  </div>
+                  {selectedRepo && (
+                    <div className="md:w-1/2 mt-8 md:mt-0">
+                      <CommitList username={userData.login} repoName={selectedRepo} />
+                    </div>
+                  )}
                 </div>
-            )}
+              )}
+            </div>
+          </main>
         </div>
-    );
+      );
   }
   
   export default Home;
--- src/services/github.service.ts
+++ src/services/github.service.ts
@@ -1,6 +1,12 @@
 /* eslint-disable no-useless-catch */
 import axios from "axios";
 
+type Repo = {
+  id: number;
+  name: string;
+  description: string;
+};
+
 const api = axios.create({
   baseURL: "http://localhost:5000/api/github",
 });
@@ -17,6 +23,18 @@ export async function getUserData(username: string) {
 export async function getUserRepos(username: string) {
   try {
     const response = await api.get(`/user/${username}/repos`);
+    const filteredRepos = response.data.filter((repo: Repo) =>
+      repo.name.toLowerCase().includes("github")
+    );
+    return filteredRepos;
+  } catch (error) {
+    throw error;
+  }
+}
+
+export async function getRepocommits(username: string, repoName: string) {
+  try {
+    const response = await api.get(`/user/${username}/${repoName}/commits`);
     return response.data;
   } catch (error) {
     throw error;
